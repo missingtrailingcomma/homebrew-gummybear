5c19e0f7f68a06386ed65adf3e9e95812c3807f1
Yizheng Shen <trailingcomma+github@gmail.com>
2024-10-22 00:13:54 -0400

    0.0.18 release

--- Formula/gummybear.rb
+++ Formula/gummybear.rb
@@ -1,57 +1,47 @@
 class Gummybear < Formula
-  desc "Gummybear is a command hosting service for ALL your commands."
-  homepage "https://github.com/missingtrailingcomma/gummybear-client"
-  url "https://github.com/missingtrailingcomma/gummybear-client/releases/download/v0.0.17/gummybear-client.tar.gz"
-  sha256 "6bc9b720abb92a20b5e0bf88df2c4ce3c56edcc81486d56cd1c4e38f5610148d"
-
-  def install
-    # install the binary
-    bin.install "gummybear"
-
-    # install the launch agent
-    bin.install "gummybear-commander"
-
-    files = [
-      "gummybear-shell-hook.sh",
-      "dependencies/bash-preexec/bash-preexec.sh",
-    ]
-
-    # Copy files and preserve directory structure
-    files.each do |file|
-      # Construct the destination path
-      dest_path = libexec/file
-
-      # Create the destination directory if it does not exist
-      mkdir_p File.dirname(dest_path)
-
-      # Copy the file to the destination
-      cp file, dest_path
-
-      # Make the file executable
-      chmod "+x", dest_path
+    desc "Gummybear is a command hosting service for ALL your commands."
+    homepage "https://github.com/missingtrailingcomma/gummybear-client"
+    url "https://github.com/missingtrailingcomma/gummybear-client/releases/download/v0.0.18/gummybear-client-source.tar.gz"
+    sha256 "aaf99a32a111022e9d54c3727fdcc5939f26bad34e270ce5bbf67560af9b093a"
+  
+    depends_on "go" => :build
+  
+    # install func should strictly mimic Makefile install
+    def install
+      system "make", "init_conf_and_log_dir"
+   
+      system "make", "binaries"
+  
+      bin.install "/tmp/gummybear/gummybear"
+      bin.install "/tmp/gummybear/gummybear-commander"
+  
+      (prefix/"share/shell-hook").mkpath
+      (prefix/"share/shell-hook").install "cli/shell-hook.sh"
+      (prefix/"share/shell-hook/dependencies/bash-preexec").mkpath
+      (prefix/"share/shell-hook/dependencies/bash-preexec").install "cli/dependencies/bash-preexec/bash-preexec.sh"
     end
-
-    mkdir_p etc/"gummybear/log"
-
-    mkdir_p etc/"gummybear/config"
-    cp "config/config.textproto", etc/"gummybear/config"
+  
+    service do
+      run [opt_bin/"gummybear-commander"]
+      keep_alive true
+      working_dir ENV["HOME"] + "/.local/share/gummybear"
+      log_path ENV["HOME"] + "/.local/share/gummybear/logs/gummybear-commander.log"
+      error_log_path ENV["HOME"] + "/.local/share/gummybear/logs/gummybear-commander-error.log"
+    end
+  
+    def caveats
+      <<~EOS
+        To complete installation , add the following line to your shell rc file:
+  
+          source #{prefix/"share/shell-hook/shell-hook.sh"}
+  
+        You will also need to restart your terminal for this change to take effect.
+      EOS
+    end
+  
+    test do
+      assert_predicate libexec/"gummybear-shell-hook.sh", :exist?, "Shell hook script not installed"
+      assert_predicate libexec/"dependencies/bash-preexec/bash-preexec.sh", :exist?, "Shell hook script not installed"
+    end
   end
-
-  service do
-    run [opt_bin/"gummybear-commander", "--homebrew_base_dir=#{HOMEBREW_PREFIX}"]
-    keep_alive true
-    working_dir var
-    log_path var/"gummybear/log/gummybear-commander.log"
-    error_log_path var/"gummybear/log/gummybear-commander-error.log"
-  end
-
-  def caveats
-    <<~EOS
-      To complete installation, add the following line to your shell rc file:
-
-        source #{libexec/"gummybear-shell-hook.sh"}
-
-      You will also need to restart your terminal for this change to take effect.
-    EOS
-  end
-end
+  
