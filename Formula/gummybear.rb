2797aeec2f1f3583760a6e9ea7c6adf53d8a9cca
Yizheng Shen <trailingcomma+github@gmail.com>
2026-04-30 22:52:37 -0400

    Update gummybear full formula

--- Formula/gummybear.rb
+++ Formula/gummybear.rb
@@ -4,46 +4,35 @@ require "fileutils"
 class Gummybear < Formula
   desc "Gummybear is a command hosting service for ALL your commands."
   homepage "https://github.com/missingtrailingcomma/gummybear-client"
-  url "https://github.com/missingtrailingcomma/gummybear-client/releases/download/v0.0.22/gummybear-client-source.tar.gz"
-  sha256 "02573b5c000fef41df405c7aae5269061f0cd6793ded25f56db5e37448c8853a"
+  url "https://github.com/missingtrailingcomma/gummybear-client/releases/download/v0.0.23/gummybear-client-source.tar.gz"
+  sha256 "f9e0a423ad2337d76d37c06b6a55798ac4d8d2cbde1ad7520e71f61c94e34aee"
 
   depends_on "go" => :build
 
   # install func should strictly mimic Makefile install
   def install
-    system "make", "init_conf_and_log_dir"
- 
-    system "make", "binaries"
-
-    bin.install "/tmp/gummybear/gummybear"
-    bin.install "/tmp/gummybear/gummybear-commander"
-
-    (prefix/"share/shell-hook").mkpath
-    (prefix/"share/shell-hook").install "cli/shell-hook.sh"
-    (prefix/"share/shell-hook/dependencies/bash-preexec").mkpath
-    (prefix/"share/shell-hook/dependencies/bash-preexec").install "cli/dependencies/bash-preexec/bash-preexec.sh"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   service do
     run [opt_bin/"gummybear-commander"]
     keep_alive true
-    working_dir ENV["HOME"] + "/.local/share/gummybear"
-    log_path ENV["HOME"] + "/.local/share/gummybear/logs/gummybear-commander.log"
-    error_log_path ENV["HOME"] + "/.local/share/gummybear/logs/gummybear-commander-error.log"
+    log_path var/"log/gummybear-commander.log"
+    error_log_path var/"log/gummybear-commander-error.log"
   end
 
   def caveats
     <<~EOS
-      To complete installation , add the following line to your shell rc file:
+      To enable Gummybear's shell capture, add the following line to your shell rc file:
 
-        source #{prefix/"share/shell-hook/shell-hook.sh"}
+        eval "$(gummybear init zsh)"  # or bash
 
       You will also need to restart your terminal for this change to take effect.
     EOS
   end
 
   test do
-    assert_predicate libexec/"gummybear-shell-hook.sh", :exist?, "Shell hook script not installed"
-    assert_predicate libexec/"dependencies/bash-preexec/bash-preexec.sh", :exist?, "Shell hook script not installed"
+    assert_match "Usage", shell_output("#{bin}/gummybear --help")
+    assert_match "Usage", shell_output("#{bin}/gummybear-commander --help")
   end
 end
